748b70739ca56b50a49e030f265ca5ab9ea8c78a
mtzeder15 <mtzeder52@gmail.com>
2025-11-28 13:20:56 -0600

    Add files via upload

---
+++ PRACTICA16/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  var helloWorld = document.getElementById("helloWorld");
+
+  helloWorld.addEventListener("click", function() {
+    alert("Hola, este es mi primer programa en JavaScript y soy Eder Martinez ");
+  });
+};
